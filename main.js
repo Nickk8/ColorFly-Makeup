edddea11217b8abc6ced88407fe36a06870cbf47
Nicolly <nicollyferreira2701@gmail.com>
2025-05-29 16:32:26 -0300

    terceiro

--- main.js
+++ main.js
@@ -13,3 +13,43 @@ window.onscroll = () => {
   menu.classList.remove('bx-x');
   navlist.classList.remove('active');
 };
+
+let carrinho = [];
+
+const botoes = document.querySelectorAll('.add-carrinho');
+
+botoes.forEach((botao) => {
+  botao.addEventListener('click', () => {
+    const produto = botao.getAttribute('data-produto');
+    const preco = parseFloat(botao.getAttribute('data-preco'));
+
+    carrinho.push({ produto, preco });
+    atualizarContadorCarrinho();
+    mostrarMensagem(`${produto} foi adicionado ao carrinho!`);
+  });
+});
+
+function atualizarContadorCarrinho() {
+  const contador = document.getElementById('cart-count');
+  contador.innerText = carrinho.length;
+}
+
+function mostrarMensagem(texto) {
+  const msg = document.getElementById('mensagem-confirmacao');
+  msg.innerText = texto;
+  msg.style.display = 'block';
+
+  setTimeout(() => {
+    msg.style.display = 'none';
+  }, 2000); // Some após 2 segundos (2000 ms)
+}
+
+
+function atualizarContadorCarrinho() {
+  const contador = document.getElementById('cart-count');
+  contador.innerText = carrinho.length;
+  
+  // Salva no localStorage
+  localStorage.setItem('carrinho', JSON.stringify(carrinho));
+}
+
